a9b3a9af6cdde88f41a5b8d8ffebc6dfd22bb630
Kaiwen Bian <biankaiwen111@berkeley.edu>
2020-05-04 23:01:12 -0700

    feat: add get_db() test

---
+++ tests/01_get_db_test.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../src")
+import InsertData
+import pymongo
+
+def test_get_db_existence():
+    """
+    Test if get_db() return the correct type
+    """
+    db = InsertData.get_db()
+    assert isinstance(db, pymongo.database.Database)
